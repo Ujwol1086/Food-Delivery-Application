93b82c5591c1f335ee10c79b42ebd29ebe5d37ce
Ujwol1086 <ujwolaryal@gmail.com>
2024-06-07 15:39:54 +0545

    Updated Signup

--- src/Pages/Auth/Create.js
+++ src/Pages/Auth/Create.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../../Components/Navbar';
 import Footer from '../../Components/Footer';
+import axios from 'axios';
 
 function Create()
 {
     const navigate = useNavigate();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) =>
+    {
+        e.preventDefault();
+        axios.post('', { name, email, password })
+            .then(result => console.log(result))
+            .catch(error => console.log(error));
+    }
     return (
         <>
             <Navbar />
@@ -15,23 +27,23 @@ function Create()
                         Already have an account? <span className="text-indigo-500 hover:text-indigo-600 cursor-pointer" onClick={() => navigate('/auth/login')}>Sign In</span>
                     </div>
                     <h2 className="text-2xl font-bold text-center">Sign Up</h2>
-                    <form className="max-w-md mx-auto my-6 grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <form className="max-w-md mx-auto my-6 grid grid-cols-1 md:grid-cols-2 gap-4" onSubmit={handleSubmit}>
                         <div className="mb-4">
                             <label htmlFor="fullName" className="block text-sm font-medium text-gray-700">Full Name</label>
-                            <input type="text" id="fullName" className="input-create" placeholder="Enter your full name" required />
+                            <input type="text" id="fullName" className="input-create" placeholder="Enter your full name" onChange={(e) => setName(e.target.value)} required />
                         </div>
                         <div className="mb-4">
                             <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-                            <input type="email" id="email" className="input-create" placeholder="Enter your email" required />
+                            <input type="email" id="email" className="input-create" placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)} required />
                         </div>
                         <div>
                             <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-                            <input type="password" id="password" className="input-create" placeholder="Enter your password" required />
+                            <input type="password" id="password" className="input-create" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)} required />
                         </div>
-                        <div>
+                        {/* <div>
                             <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">Confirm Password</label>
                             <input type="password" id="confirmPassword" className="input-create" placeholder="Confirm your password" required />
-                        </div>
+                        </div> */}
                     </form>
                     <div className="max-w-md mx-auto">
                         <div className="flex items-center justify-between mb-4">
--- src/Pages/Auth/Login.js
+++ src/Pages/Auth/Login.js
@@ -6,6 +6,7 @@ import Footer from '../../Components/Footer';
 function Login()
 {
     const navigate = useNavigate();
+
     return (
         <>
             <Navbar />
