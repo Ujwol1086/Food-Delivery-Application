b36a93454ba62f22ab3f81e8319ff791fc4edf83
Ujwol1086 <ujwolaryal@gmail.com>
2024-06-07 20:16:17 +0545

    Create JS

--- src/Pages/Auth/Create.js
+++ src/Pages/Auth/Create.js
@@ -14,7 +14,7 @@ function Create()
     const handleSubmit = (e) =>
     {
         e.preventDefault();
-        axios.post('', { name, email, password })
+        axios.post('https://localhost:3001/auth/create', { name, email, password })
             .then(result => console.log(result))
             .catch(error => console.log(error));
     }
@@ -44,18 +44,18 @@ function Create()
                             <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">Confirm Password</label>
                             <input type="password" id="confirmPassword" className="input-create" placeholder="Confirm your password" required />
                         </div> */}
-                    </form>
-                    <div className="max-w-md mx-auto">
-                        <div className="flex items-center justify-between mb-4">
-                            <div className="flex items-center">
-                                <input id="terms" name="terms" type="checkbox" className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" required />
-                                <label htmlFor="terms" className="ml-2 block text-sm text-gray-900">Accept Terms & Conditions</label>
+                        <div className="max-w-md mx-auto">
+                            {/* <div className="flex items-center justify-between mb-4">
+                                <div className="flex items-center">
+                                    <input id="terms" name="terms" type="checkbox" className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" required />
+                                    <label htmlFor="terms" className="ml-2 block text-sm text-gray-900">Accept Terms & Conditions</label>
+                                </div>
+                            </div> */}
+                            <div className='max-w-xl mx-auto'>
+                                <button type="submit" className="w-full md:w-3/5 flex justify-center mt-4 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Create Account</button>
                             </div>
                         </div>
-                        <div className='max-w-xl mx-auto'>
-                            <button type="submit" className="w-full md:w-3/5 flex justify-center mt-4 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Create Account</button>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div >
             <Footer />
